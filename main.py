555a99ddc1b638d4a078038af64dff965c62066c
Abdullah-Asad <75277035+Abdullah-Asad@users.noreply.github.com>
2020-12-18 19:33:56 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-
+import cv2
 from flask import Flask, request
+import tensorflow as tf
+
+def prepare(file):
+    IMG_SIZE = 50
+    new_array = cv2.resize(file, (IMG_SIZE, IMG_SIZE))
+    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+
+
+def load_model():
+    global model
+
+    model = tf.keras.models.load_model("CNN.h5")
+
 
 app = Flask(__name__)
 
