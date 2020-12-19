ac7afe1c230282512cee8a3aac8d797338fce64d
Abdullah-Asad <75277035+Abdullah-Asad@users.noreply.github.com>
2020-12-19 06:22:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 from flask import Flask, request
-import tensorflow as tf
+
 
 def prepare(file):
     IMG_SIZE = 50
@@ -8,10 +8,7 @@ def prepare(file):
     return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 
 
-def load_model():
-    global model
 
-    model = tf.keras.models.load_model("CNN.h5")
 
 
 app = Flask(__name__)
